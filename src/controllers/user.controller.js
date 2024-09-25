6d3756719a384827e2c90dcbda6650a9acc9cefc
Harish <bhattaharish10@gmail.com>
2024-09-25 21:09:48 +0545

    Register steps

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Registered" });
+  // Get the data from the user
+  //Check if the user already exists
+  //Check if the values are correcy
+  //Save values to the database if correct
+  //Send response to the user
 });
 
 export { registerUser };
