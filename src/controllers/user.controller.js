4005d59d57b85ca83f17f74f7f50aaf5aa3ba4ca
Harish <bhattaharish10@gmail.com>
2024-09-29 00:17:59 +0545

    Changes response of the get channel from aray to object

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -431,7 +431,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, user, "Channel fetched successfully"));
+    .json(new ApiResponse(200, user[0], "Channel fetched successfully"));
 });
 
 export {
